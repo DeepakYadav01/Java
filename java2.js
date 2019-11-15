6f777b098dc46d0368b7e2c8cb0c1c756cb5e5b3
DeepaYadav01 <dy79402@gmail.com>
2019-11-15 14:38:47 +0530

    Print starting and ending indices of subarrays having their sum equal to the given integer(entered by user)

---
+++ java2.js
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SubArrayKIndex1 {
+  public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		 int n;
+		  System.out.println("Enter the size of array");
+		   n=sc.nextInt();
+		   int a[]=new int[n];
+		  System.out.println("Enter elements");
+
+		  for(int i=0;i<n;i++)
+		  {
+			a[i]=sc.nextInt();
+		  }
+
+		System.out.println("Enter number");
+		int sum=0,ind;
+		int m=sc.nextInt();
+
+		for(int i=0;i<n;i++)
+	   	{
+			for(int j=i;j<n;j++)
+			{
+				sum=sum+a[j];
+				if(sum==m)
+				{
+					System.out.println("Starting index :"+i+","+"Ending index :"+j);
+
+				}
+
+		    }
+				sum=0;
+	    }
+
+    }
+
+}
