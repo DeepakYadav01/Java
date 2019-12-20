1aa8306e502a8caec55f2d90452a2f07269bd37f
DeepaYadav01 <dy79402@gmail.com>
2019-12-20 23:13:28 +0530

    Count no of objects using static

---
+++ java24.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    static int a=0;
+    Main()
+    {
+        a++;
+    }
+	public static void main(String[] args) {
+	   
+      Main m = new Main();
+            Main m1 = new Main();
+
+     System.out.println(a);
+
+	}
+}
